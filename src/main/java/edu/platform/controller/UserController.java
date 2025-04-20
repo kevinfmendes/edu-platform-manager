2efc4983c19fac285631731b78fb07e740baf5b3
kevinfmendes <keviin.nicolas18@gmail.com>
2025-04-20 16:57:04 -0300

    feat(Usuarios): add roles e anotacoes para esquema de autorizacao

--- src/main/java/edu/platform/controller/UserController.java
+++ src/main/java/edu/platform/controller/UserController.java
@@ -2,17 +2,19 @@ package edu.platform.controller;
 
 import edu.platform.entity.UserEntity;
 import edu.platform.service.UserService;
+import io.quarkus.security.Authenticated;
+import jakarta.annotation.security.RolesAllowed;
 import jakarta.transaction.Transactional;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 
-import java.util.List;
 import java.util.UUID;
 
 @Path("/users")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
+@Authenticated
 public class UserController {
 
     private final UserService userService;
@@ -22,6 +24,7 @@ public class UserController {
     }
 
     @GET
+    @RolesAllowed("USER")
     public Response findAllUsers(@QueryParam("page") @DefaultValue("0") Integer page,
                                  @QueryParam("pageSize") @DefaultValue("5") Integer pageSize) {
         var users = userService.findAllUsers(page, pageSize);
@@ -30,12 +33,14 @@ public class UserController {
 
     @Path("/{id}")
     @GET
+    @RolesAllowed("USER")
     public Response findUserById(@PathParam("id") UUID id) {
         return Response.ok(userService.findById(id)).build();
     }
 
     @POST
     @Transactional
+    @RolesAllowed("ADMIN")
     public Response createUser(UserEntity user) {
         return Response.ok(userService.createUser(user)).build();
     }
@@ -43,6 +48,7 @@ public class UserController {
     @Path("/{id}")
     @PUT
     @Transactional
+    @RolesAllowed("ADMIN")
     public Response updateUser(@PathParam("id") UUID id, UserEntity user) {
         return Response.ok(userService.updateUser(id, user)).build();
     }
@@ -50,6 +56,7 @@ public class UserController {
     @Path("/{id}")
     @DELETE
     @Transactional
+    @RolesAllowed("ADMIN")
     public Response deleteUserById(@PathParam("id") UUID id) {
         userService.deleteUserById(id);
         return Response.noContent().build();
